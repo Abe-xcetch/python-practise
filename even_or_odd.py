1229affa3e069b55f5c3eabbf4870821c885572e
Abe-xcetch <Kilwakeabe4@gmail.com>
2025-04-26 23:11:16 +0300

    even_or_odd

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+# Task: Check if a number is even or odd
+# Thought process: Use modulus (%) to check if number divisible by 2
+
+number = 7
+
+if number % 2 == 0:
+    print(number, "is Even")
+else:
+    print(number, "is Odd")
